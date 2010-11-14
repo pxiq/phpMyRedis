ef673e49460e48a3a2ff89b8f22da005864badae
colummcgaley <colum@gmx.com>
2010-11-13 20:13:14 -0500

    Changes

--- index.php
+++ index.php
@@ -1,8 +1,29 @@
 <?php
+# Copyright (c) 2010, Colum McGaley
+# All rights reserved.
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 	session_start();
 	
-	include "backend/core.php";
+	include "backend/core.lib.php";
 	include "backend/Predis.php";
+	
+	if($_GET['d'] == "true") {
+		unset($_SESSION["phpmyredis.session"]);
+	}
+	
 	if(isset($_SESSION["phpmyredis.session"])) {
 		$s = array(
 			"host" => $_SESSION['phpmyredis.host'],
@@ -12,9 +33,20 @@
 		);
 		$redis = new Predis_Client(array(
 		    'host'     => $s['host'],
-		    'port'	   => $s['port'], 
 		    'password' => $s['password'], 
 		    'database' => $s['database'], 
 		));
+		# Because Predis does not thorw an error when the class is created
+		# We can check because this command will return an array if true
+		# String if it is false
+		$cmdSet = $redis->createCommand('keys');
+		$cmdSet->setArguments('*');
+		@$cmdGetReply = $redis->executeCommand($cmdSet);
+		if(!is_array($cmdGetReply))
+			echo "Login Fail";
+		else
+			$core->runWithConnection();
+	} else {
+		$core->defineStatic("login");
 	}
 ?>
