afe3cdc9a6c7a6d6b65b1e8aede2321854775d56
Colum McGaley <c.mcgaley@gmail.com>
2010-12-27 19:06:12 -0500

    Added list function

---
+++ backend/static/list.tpl.php
@@ -0,0 +1,31 @@
+			    <div id="sidebar">
+                	<ul class="sideNav">
+                    	<li><a href="?rf=list">Select Key</a></li>
+                    	<li><a href="?rf=new">Create a new key</a></li>
+                    	<li><a href="?rf=list&keys=post" title="Allows you to change the prameters to the keys command">Change Sort</a></li>
+                    </ul>
+                    <!-- // .sideNav -->
+            	</div>    
+                <!-- // #sidebar -->
+
+                <div id="main">
+                	<h3>Select A Key</h3>
+                	<table cellpadding="0" cellspacing="0">
+<?php
+$cmdSet = $redis->createCommand('keys');
+if(isset($_POST['keys.manualInput'])) {
+	$cmdSet->setArguments($_POST['keys.manualInput']);
+} else {
+	$cmdSet->setArguments('*');
+}
+$cmdGetReply = $redis->executeCommand($cmdSet);
+
+foreach($cmdGetReply as $data) {
+	echo "	<tr>
+		<td width=\"543\">$data</td>
+		<td width=\"155\" class=\"action\"><a href=\"?f=view&view=$data\" class=\"view\">View</a><a href=\"?f=edit&view=$data\" class=\"edit\">Edit</a><a href=\"?f=view&view=$data&do=del\" class=\"delete\" onclick=\"warn($data)\">Delete</a></td>
+	</tr>";
+}
+?>
+					</table>
+				</div>
